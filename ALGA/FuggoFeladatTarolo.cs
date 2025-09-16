c95fa13ced763473c4bcf7ab805a29564676af56
bodnarlevente <bodilevi3@gmail.com>
2025-09-16 23:48:23 +0200

    kedd este

---
+++ ALGA/FuggoFeladatTarolo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OE.ALGA
+{
+    abstract class FuggoFeladatTarolo<T> : FeladatTarolo<T> where T : IVegrehajto, IFuggo
+    {
+        public FuggoFeladatTarolo(int meret) : base(meret)
+        {
+        }
+        public void FuggosegSzerintVegrehajt()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (tarolo[i].FuggosegTeljesul)
+                {
+                    tarolo[i].Vegrehajtas();
+                }
+            }
+        }
+
+    }
+}
