dca64f4af118961b783fb1b836c99bc5134858d3
bodnarlevente <bodilevi3@gmail.com>
2025-09-16 22:24:18 +0200

    tarolo megtelt exc

---
+++ ALGA/TaroloMegteltException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OE.ALGA
+{
+    internal class TaroloMegteltException : Exception
+    {
+        public TaroloMegteltException() : base("A tároló megtelt.")
+        {
+        }
+        public TaroloMegteltException(string message) : base(message)
+        {
+        }
+        public TaroloMegteltException(string message, Exception inner) : base(message, inner)
+        {
+        }
+        protected TaroloMegteltException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+    }
+}
