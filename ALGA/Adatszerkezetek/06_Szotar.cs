15b8ac95b34684bb492a056cdbf9008567453478
bodnarlevente <bodilevi3@gmail.com>
2025-10-26 16:17:28 +0100

    6elso

--- ALGA/Adatszerkezetek/06_Szotar.cs
+++ ALGA/Adatszerkezetek/06_Szotar.cs
@@ -1,6 +1,117 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OE.ALGA.Adatszerkezetek
 {
-    // 6. heti labor feladat - Tesztek: 06_SzotarTesztek.cs
+  
+
+    
+    internal class SzotarElem<K, T>
+    {
+        public K kulcs;
+        public T tart;
+
+        public SzotarElem(K kulcs, T tart)
+        {
+            this.kulcs = kulcs;
+            this.tart = tart;
+        }
+    }
+
+    public class HasitoSzotarTulcsordulasiTerulettel<K, T> : Szotar<K, T>
+    {
+        private SzotarElem<K, T>?[] E;
+        private Func<K, int> h;
+        private LinkedList<SzotarElem<K, T>> U;
+
+        public HasitoSzotarTulcsordulasiTerulettel(int meret, Func<K, int> hasitoFuggveny)
+        {
+            E = new SzotarElem<K, T>?[meret];
+            U = new LinkedList<SzotarElem<K, T>>();
+            h = x => Math.Abs(hasitoFuggveny(x)) % E.Length;
+        }
+
+        public HasitoSzotarTulcsordulasiTerulettel(int meret)
+            : this(meret, x => x.GetHashCode())
+        {
+        }
+
+        private SzotarElem<K, T>? KulcsKeres(K kulcs)
+        {
+            int i = h(kulcs);
+            if (E[i] != null && E[i].kulcs.Equals(kulcs))
+            {
+                return E[i];
+            }
+
+            foreach (SzotarElem<K, T> elem in U)
+            {
+                if (elem.kulcs.Equals(kulcs))
+                {
+                    return elem;
+                }
+            }
+
+            return null;
+        }
+
+        public void Beir(K kulcs, T ertek)
+        {
+            SzotarElem<K, T>? elem = KulcsKeres(kulcs);
+            if (elem != null)
+            {
+                elem.tart = ertek;
+                return;
+            }
+
+            SzotarElem<K, T> ujElem = new SzotarElem<K, T>(kulcs, ertek);
+            int i = h(kulcs);
+            if (E[i] == null)
+            {
+                E[i] = ujElem;
+            }
+            else
+            {
+                U.AddLast(ujElem);
+            }
+        }
+
+        public T Kiolvas(K kulcs)
+        {
+            SzotarElem<K, T>? elem = KulcsKeres(kulcs);
+            if (elem != null)
+            {
+                return elem.tart;
+            }
+            throw new HibasKulcsKivetel();
+        }
+
+        public void Torol(K kulcs)
+        {
+            int i = h(kulcs);
+            if (E[i] != null && E[i].kulcs.Equals(kulcs))
+            {
+                E[i] = null;
+                return;
+            }
+
+            SzotarElem<K, T>? torlendo = null;
+            foreach (var elem in U)
+            {
+                if (elem.kulcs.Equals(kulcs))
+                {
+                    torlendo = elem;
+                    break;
+                }
+            }
+
+            if (torlendo != null)
+            {
+                U.Remove(torlendo);
+                return;
+            }
+
+            throw new HibasKulcsKivetel();
+        }
+    }
 }
