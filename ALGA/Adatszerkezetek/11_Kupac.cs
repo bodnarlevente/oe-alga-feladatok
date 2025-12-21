3293986c527eaf16398acfcdbf95bbca0fd83c96
bodnarlevente <bodilevi3@gmail.com>
2025-12-21 03:24:44 +0100

    12 bütyk

--- ALGA/Adatszerkezetek/11_Kupac.cs
+++ ALGA/Adatszerkezetek/11_Kupac.cs
@@ -1,46 +1,172 @@
-﻿//using System;
+﻿using System;
 
-//namespace OE.ALGA.Adatszerkezetek
-//{
-//    public class SulyozotlanGraf
-//    {
-//        private readonly bool[,] szomszedsagiMatrix;
-//        public int CsucsokSzama { get; }
+namespace OE.ALGA.Adatszerkezetek
+{
+   
 
-//        public SulyozotlanGraf(int csucsokSzama)
-//        {
-//            if (csucsokSzama <= 0)
-//            {
-//                throw new ArgumentOutOfRangeException(nameof(csucsokSzama), "A csúcsok száma pozitív egész szám kell legyen.");
-//            }
+    public class Kupac<T>
+    {
+        protected T[] E;
+        protected int n;
+        protected Func<T, T, bool> nagyobb;
 
-//            CsucsokSzama = csucsokSzama;
-//            szomszedsagiMatrix = new bool[csucsokSzama, csucsokSzama];
-//        }
+        public Kupac(T[] E, int n, Func<T, T, bool> nagyobbPrioritas)
+        {
+            this.E = E;
+            this.n = n;
+            this.nagyobb = nagyobbPrioritas;
+            KupacotEpit();
+        }
 
-//        public void Elerheto(int forras, int cel)
-//        {
-//            EllenorizCsucsIndex(forras);
-//            EllenorizCsucsIndex(cel);
-//            szomszedsagiMatrix[forras, cel] = true;
-//            szomszedsagiMatrix[cel, forras] = true; // Mivel irányítatlan graf
-//        }
+        public static int Bal(int i)
+        {
+            return 2 * i + 1;
+        }
 
-//        public bool ElerhetoE(int forras, int cel)
-//        {
-//            EllenorizCsucsIndex(forras);
-//            EllenorizCsucsIndex(cel);
-//            return szomszedsagiMatrix[forras, cel];
-//        }
+        public static int Jobb(int i)
+        {
+            return 2 * i + 2;
+        }
 
-//        private void EllenorizCsucsIndex(int index)
-//        {
-//            if (index < 0 || index >= CsucsokSzama)
-//            {
-//                throw new ArgumentOutOfRangeException(nameof(index), "A csúcs indexe a megadott tartományon kívül esik.");
-//            }
-//        }
-//    }
+        public static int Szulo(int i)
+        {
+            return (i - 1) / 2;
+        }
 
+        protected void Kupacol(int i)
+        {
+            int b = Bal(i);
+            int j = Jobb(i);
+            int max = i;
 
-//}
+            if (b < n && nagyobb(E[b], E[max]))
+            {
+                max = b;
+            }
+
+            if (j < n && nagyobb(E[j], E[max]))
+            {
+                max = j;
+            }
+
+            if (max != i)
+            {
+                T temp = E[i];
+                E[i] = E[max];
+                E[max] = temp;
+                Kupacol(max);
+            }
+        }
+
+        protected void KupacotEpit()
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Kupacol(i);
+            }
+        }
+    }
+
+    public class KupacRendezes<T> : Kupac<T> where T : IComparable<T>
+    {
+        public KupacRendezes(T[] A)
+            : base(A, A.Length, (x, y) => x.CompareTo(y) > 0)
+        {
+        }
+
+        public void Rendezes()
+        {
+            int eredetiN = n;
+            for (int i = n - 1; i > 0; i--)
+            {
+                T temp = E[0];
+                E[0] = E[i];
+                E[i] = temp;
+                n--;
+                Kupacol(0);
+            }
+            n = eredetiN;
+        }
+    }
+
+    public class KupacPrioritasosSor<T> : Kupac<T>, PrioritasosSor<T>
+    {
+        public KupacPrioritasosSor(int meret, Func<T, T, bool> nagyobbPrioritas)
+            : base(new T[meret], 0, nagyobbPrioritas)
+        {
+        }
+
+        public bool Ures
+        {
+            get { return n == 0; }
+        }
+
+        private void KulcsotFelvisz(int i)
+        {
+            while (i > 0 && nagyobb(E[i], E[Szulo(i)]))
+            {
+                int sz = Szulo(i);
+                T temp = E[i];
+                E[i] = E[sz];
+                E[sz] = temp;
+                i = sz;
+            }
+        }
+
+        public void Sorba(T ertek)
+        {
+            if (n >= E.Length)
+            {
+                throw new NincsHelyKivetel();
+            }
+
+            E[n] = ertek;
+            n++;
+            KulcsotFelvisz(n - 1);
+        }
+
+        public T Sorbol()
+        {
+            if (n == 0)
+            {
+                throw new NincsElemKivetel();
+            }
+
+            T elso = E[0];
+            E[0] = E[n - 1];
+            n--;
+            Kupacol(0);
+            return elso;
+        }
+
+        public T Elso()
+        {
+            if (n == 0)
+            {
+                throw new NincsElemKivetel();
+            }
+            return E[0];
+        }
+
+        public void Frissit(T ertek)
+        {
+            int index = -1;
+            for (int i = 0; i < n; i++)
+            {
+                if (E[i].Equals(ertek))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index == -1)
+            {
+                throw new NincsElemKivetel();
+            }
+
+            KulcsotFelvisz(index);
+            Kupacol(index);
+        }
+    }
+}
--- ALGA/Adatszerkezetek/12_SulyozottGraf.cs
+++ ALGA/Adatszerkezetek/12_SulyozottGraf.cs
@@ -1,44 +1,292 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OE.ALGA.Adatszerkezetek
 {
-    public class SulyozottGraf
+    public static class Kiterjesztesek
     {
-        private readonly int?[,] szomszedsagiMatrix;
-        public int CsucsokSzama { get; }
-
-        public SulyozottGraf(int csucsokSzama)
+        public static T[] Tombbe<T>(this Halmaz<T> halmaz)
         {
-            if (csucsokSzama <= 0)
+            List<T> lista = new List<T>();
+            halmaz.Bejar(x => lista.Add(x));
+            return lista.ToArray();
+        }
+    }
+
+    public class SulyozottEgeszGrafEl : EgeszGrafEl, SulyozottGrafEl<int>
+    {
+        public float Suly { get; private set; }
+
+        public SulyozottEgeszGrafEl(int honnan, int hova, float suly) : base(honnan, hova)
+        {
+            Suly = suly;
+        }
+    }
+
+    public class CsucsmatrixSulyozottEgeszGraf : SulyozottGraf<int, SulyozottEgeszGrafEl>
+    {
+        private int n;
+        private float[,] M;
+
+        public int CsucsokSzama
+        {
+            get { return n; }
+        }
+
+        public int ElekSzama
+        {
+            get
             {
-                throw new ArgumentOutOfRangeException(nameof(csucsokSzama), "A csúcsok száma pozitív egész szám kell legyen.");
+                int count = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (!float.IsNaN(M[i, j]))
+                        {
+                            count++;
+                        }
+                    }
+                }
+                return count;
+            }
+        }
+
+        public Halmaz<int> Csucsok
+        {
+            get
+            {
+                FaHalmaz<int> csucsok = new FaHalmaz<int>();
+                for (int i = 0; i < n; i++)
+                {
+                    csucsok.Beszur(i);
+                }
+                return csucsok;
+            }
+        }
+
+        public Halmaz<SulyozottEgeszGrafEl> Elek
+        {
+            get
+            {
+                FaHalmaz<SulyozottEgeszGrafEl> elek = new FaHalmaz<SulyozottEgeszGrafEl>();
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (!float.IsNaN(M[i, j]))
+                        {
+                            elek.Beszur(new SulyozottEgeszGrafEl(i, j, M[i, j]));
+                        }
+                    }
+                }
+                return elek;
+            }
+        }
+
+        public CsucsmatrixSulyozottEgeszGraf(int n)
+        {
+            this.n = n;
+            M = new float[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    M[i, j] = float.NaN;
+                }
+            }
+        }
+
+        public void UjEl(int honnan, int hova, float suly)
+        {
+            M[honnan, hova] = suly;
+        }
+
+        public bool VezetEl(int honnan, int hova)
+        {
+            return !double.IsNaN(M[honnan, hova]);
+        }
+
+        public float Suly(int honnan, int hova)
+        {
+            if (float.IsNaN(M[honnan, hova]))
+            {
+                throw new NincsElemKivetel();
+            }
+            return M[honnan, hova];
+        }
+
+        public Halmaz<int> Szomszedai(int csucs)
+        {
+            FaHalmaz<int> szomszedok = new FaHalmaz<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (!double.IsNaN(M[csucs, i]))
+                {
+                    szomszedok.Beszur(i);
+                }
+            }
+            return szomszedok;
+        }
+    }
+
+
+    public class Utkereses
+    {
+        public static Szotar<V, double> Dijkstra<V, E>(SulyozottGraf<V, E> g, V start)
+            where E : SulyozottGrafEl<V>
+            where V : IComparable<V>
+        {
+            HasitoSzotarTulcsordulasiTerulettel<V, double> d = new HasitoSzotarTulcsordulasiTerulettel<V, double>(g.ElekSzama);
+            V[] csucsokTomb = g.Csucsok.Tombbe();
+
+            for (int i = 0; i < csucsokTomb.Length; i++)
+            {
+                d.Beir(csucsokTomb[i], double.PositiveInfinity);
+            }
+            d.Beir(start, 0);
+
+            KupacPrioritasosSor<V> S = new KupacPrioritasosSor<V>(
+                g.CsucsokSzama,
+                (v1, v2) => d.Kiolvas(v1) < d.Kiolvas(v2)
+            );
+
+            for (int i = 0; i < csucsokTomb.Length; i++)
+            {
+                S.Sorba(csucsokTomb[i]);
             }
 
-            CsucsokSzama = csucsokSzama;
-            szomszedsagiMatrix = new int?[csucsokSzama, csucsokSzama];
-        }
-
-        public void Elerheto(int forras, int cel, int suly)
-        {
-            EllenorizCsucsIndex(forras);
-            EllenorizCsucsIndex(cel);
-            szomszedsagiMatrix[forras, cel] = suly;
-            szomszedsagiMatrix[cel, forras] = suly; // Mivel irányítatlan graf
-        }
-
-        public int? ElerhetoE(int forras, int cel)
-        {
-            EllenorizCsucsIndex(forras);
-            EllenorizCsucsIndex(cel);
-            return szomszedsagiMatrix[forras, cel];
-        }
-
-        private void EllenorizCsucsIndex(int index)
-        {
-            if (index < 0 || index >= CsucsokSzama)
+            while (!S.Ures)
             {
-                throw new ArgumentOutOfRangeException(nameof(index), "A csúcs indexe a megadott tartományon kívül esik.");
+                V u = S.Sorbol();
+                V[] szomszedok = g.Szomszedai(u).Tombbe();
+
+                for (int i = 0; i < szomszedok.Length; i++)
+                {
+                    V v = szomszedok[i];
+                    double tavolsag = d.Kiolvas(u) + g.Suly(u, v);
+                    if (tavolsag < d.Kiolvas(v))
+                    {
+                        d.Beir(v, tavolsag);
+                        S.Frissit(v);
+                    }
+                }
             }
+
+            return d;
+        }
+    }
+
+    public class FeszitofaKereses
+    {
+        public static Szotar<V, V> Prim<V, E>(SulyozottGraf<V, E> g, V start)
+            where V : IComparable<V>
+            where E : SulyozottGrafEl<V>
+        {
+            HasitoSzotarTulcsordulasiTerulettel<V, float> d = new HasitoSzotarTulcsordulasiTerulettel<V, float>(g.ElekSzama);
+            HasitoSzotarTulcsordulasiTerulettel<V, V> pi = new HasitoSzotarTulcsordulasiTerulettel<V, V>(g.ElekSzama);
+            FaHalmaz<V> H = new FaHalmaz<V>();
+            V[] csucsokTomb = g.Csucsok.Tombbe();
+
+            for (int i = 0; i < csucsokTomb.Length; i++)
+            {
+                V v = csucsokTomb[i];
+                d.Beir(v, float.PositiveInfinity);
+                H.Beszur(v);
+            }
+            d.Beir(start, 0);
+
+            KupacPrioritasosSor<V> S = new KupacPrioritasosSor<V>(
+                g.CsucsokSzama,
+                (v1, v2) => d.Kiolvas(v1) < d.Kiolvas(v2)
+            );
+
+            for (int i = 0; i < csucsokTomb.Length; i++)
+            {
+                S.Sorba(csucsokTomb[i]);
+            }
+
+            while (!S.Ures)
+            {
+                V u = S.Sorbol();
+                H.Torol(u);
+
+                V[] szomszedok = g.Szomszedai(u).Tombbe();
+                for (int i = 0; i < szomszedok.Length; i++)
+                {
+                    V v = szomszedok[i];
+                    if (H.Eleme(v) && g.Suly(u, v) < d.Kiolvas(v))
+                    {
+                        d.Beir(v, g.Suly(u, v));
+
+                        try
+                        {
+                            pi.Torol(v);
+                        }
+                        catch { }
+                        pi.Beir(v, u);
+
+                        S.Frissit(v);
+                    }
+                }
+            }
+
+            return pi;
+        }
+
+        public static Halmaz<E> Kruskal<V, E>(SulyozottGraf<V, E> g, V start)
+            where E : SulyozottGrafEl<V>, IComparable<E>
+            where V : IComparable<V>
+        {
+            FaHalmaz<E> A = new FaHalmaz<E>();
+            HasitoSzotarTulcsordulasiTerulettel<V, V> szulo = new HasitoSzotarTulcsordulasiTerulettel<V, V>(g.ElekSzama);
+
+            V[] csucsok = g.Csucsok.Tombbe();
+            for (int i = 0; i < csucsok.Length; i++)
+            {
+                szulo.Beir(csucsok[i], csucsok[i]);
+            }
+
+            List<E> elekLista = new List<E>();
+            E[] elekTomb = g.Elek.Tombbe();
+            for (int i = 0; i < elekTomb.Length; i++)
+            {
+                elekLista.Add(elekTomb[i]);
+            }
+
+            elekLista.Sort((e1, e2) => e1.Suly.CompareTo(e2.Suly));
+
+            foreach (E el in elekLista)
+            {
+                V u = el.Honnan;
+                V v = el.Hova;
+
+                V gyokerU = HolVan(szulo, u);
+                V gyokerV = HolVan(szulo, v);
+
+                if (!gyokerU.Equals(gyokerV))
+                {
+                    A.Beszur(el);
+                    Unio(szulo, gyokerU, gyokerV);
+                }
+            }
+
+            return A;
+        }
+
+        private static V HolVan<V>(Szotar<V, V> szulo, V v) where V : IComparable<V>
+        {
+            V aktualis = v;
+            while (!szulo.Kiolvas(aktualis).Equals(aktualis))
+            {
+                aktualis = szulo.Kiolvas(aktualis);
+            }
+            return aktualis;
+        }
+
+        private static void Unio<V>(HasitoSzotarTulcsordulasiTerulettel<V, V> szulo, V u, V v) where V : IComparable<V>
+        {
+            szulo.Beir(u, v);
         }
     }
 }
