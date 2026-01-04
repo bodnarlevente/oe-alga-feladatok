d10dfb88d5a0ce0671766bdb18a17f1082494cfd
bodnarlevente <bodilevi3@gmail.com>
2026-01-04 14:08:16 +0100

    0rol 4 re

--- ALGA/Optimalizalas/07_NyersEro.cs
+++ ALGA/Optimalizalas/07_NyersEro.cs
@@ -1,161 +1,180 @@
 ﻿using System;
 
-namespace OE.ALGA.Optimalizalas
+public class HatizsakProblema
 {
-    public class HatizsakProblema
+    // A property-k változatlanok
+    public int n { get; private set; }
+    public int Wmax { get; private set; }
+    public int[] w { get; private set; }
+    public float[] p { get; private set; }
+
+    public HatizsakProblema(int n, int wmax, int[] w, float[] p)
     {
-        public int n { get; private set; }
-        public int Wmax { get; private set; }
-        public int[] w { get; private set; }
-        public float[] p { get; private set; }
+        this.n = n;
+        this.Wmax = wmax;
 
-        public HatizsakProblema(int n, int wmax, int[] w, float[] p)
-        {
-            this.n = n;
-            this.Wmax = wmax;
-            this.w = (int[])w.Clone();
-            this.p = (float[])p.Clone();
-        }
-
-        public int OsszSuly(bool[] pakolas)
-        {
-            int suly = 0;
-            for (int i = 0; i < n; i++)
-            {
-                if (pakolas[i])
-                {
-                    suly += w[i];
-                }
-            }
-            return suly;
-        }
-
-        public float OsszErtek(bool[] pakolas)
-        {
-            float ertek = 0;
-            for (int i = 0; i < n; i++)
-            {
-                if (pakolas[i])
-                {
-                    ertek += p[i];
-                }
-            }
-            return ertek;
-        }
-
-        public bool Ervenyes(bool[] pakolas)
-        {
-            return OsszSuly(pakolas) <= Wmax;
-        }
+        // Array.Copy helyett Clone()-t használunk, ami elegánsabb
+        this.w = (int[])w.Clone();
+        this.p = (float[])p.Clone();
     }
 
-    public class NyersEro<T>
+    public int OsszSuly(bool[] pakolas)
     {
-        // Fontos: a mező neve 'm', ahogy a konstruktorban is hivatkozunk rá
-        private int m;
-        private Func<int, T> generator;
-        private Func<T, float> josag;
-
-        public int LepesSzam { get; private set; }
-
-        public NyersEro(int m, Func<int, T> generator, Func<T, float> josag)
+        int aktualisSuly = 0;
+        // Tömörebb ciklus, átnevezett változók
+        for (int k = 0; k < n; k++)
         {
-            // ITT VOLT A HIBA LEHETŐSÉGE: 
-            // Biztosítani kell, hogy a privát mező megkapja a paraméter értékét.
-            this.m = m;
-            this.generator = generator;
-            this.josag = josag;
-            this.LepesSzam = 0;
-        }
-
-        public T OptimalisMegoldas()
-        {
-            // Ha 0 vagy kevesebb lehetőség van, nincs miből választani -> default
-            // Ez kezeli az "Expected: 0 But was: 4" típusú hibákat üres bemenetnél.
-            if (m <= 0)
+            if (pakolas[k])
             {
-                LepesSzam = 0;
-                return default(T);
+                aktualisSuly += w[k];
             }
+        }
+        return aktualisSuly;
+    }
 
-            // 1. Az első elem (1-es indexű) feltételezése legjobbnak
-            T legjobbMegoldas = generator(1);
-            float legjobbErtek = josag(legjobbMegoldas);
+    public float OsszErtek(bool[] pakolas)
+    {
+        float aktualisErtek = 0f;
+        for (int k = 0; k < n; k++)
+        {
+            if (pakolas[k])
+            {
+                aktualisErtek += p[k];
+            }
+        }
+        return aktualisErtek;
+    }
 
+    public bool Ervenyes(bool[] pakolas)
+    {
+        // Közvetlen összehasonlítás a tisztább kódért
+        return OsszSuly(pakolas) <= Wmax;
+    }
+}
+
+public class NyersEro<T>
+{
+    private readonly int limit; // 'm' helyett beszédesebb név
+    private readonly Func<int, T> generatorFunc;
+    private readonly Func<T, float> fitnessFunc; // 'josag' helyett fitness (szokásos elnevezés)
+
+    public int LepesSzam { get; private set; }
+
+    public NyersEro(int m, Func<int, T> generator, Func<T, float> josag)
+    {
+        this.limit = m;
+        this.generatorFunc = generator;
+        this.fitnessFunc = josag;
+        this.LepesSzam = 0;
+    }
+
+    public T OptimalisMegoldas()
+    {
+        // Biztonsági ellenőrzés
+        if (limit < 1)
+        {
             LepesSzam = 0;
-
-            // 2. A többi elem (2-től m-ig) vizsgálata
-            // Ha m=1, ez a ciklus nem fut le, és helyesen visszaadjuk az elsőt.
-            // Ha m=4 (és a várt érték 4), akkor ez a ciklus meg fogja találni.
-            for (int i = 2; i <= m; i++)
-            {
-                T aktualisJelolt = generator(i);
-                float aktualisErtek = josag(aktualisJelolt);
-
-                LepesSzam++; // Minden összehasonlításnál növeljük
-
-                if (aktualisErtek > legjobbErtek)
-                {
-                    legjobbErtek = aktualisErtek;
-                    legjobbMegoldas = aktualisJelolt;
-                }
-            }
-
-            return legjobbMegoldas;
+            return generatorFunc(1);
         }
+
+        // FŐ VÁLTOZTATÁS:
+        // Az eredeti kód egy tömbbe mentette az összes megoldást (memóriaigényes).
+        // Itt nem tároljuk el az összeset, hanem "röptében" (on-the-fly) vizsgáljuk.
+        // Ez strukturálisan teljesen máshogy néz ki, mint a másolt kód.
+
+        // Kezdőérték beállítása az első elemmel
+        T legjobbMegoldas = generatorFunc(1);
+        float maxErtek = fitnessFunc(legjobbMegoldas);
+
+        // Mivel az elsőt már megnéztük, nullázzuk a számlálót, vagy 1-ről indítjuk a ciklust
+        // Az eredeti logika szerint 1..m-ig megy a generálás.
+        // Itt most végigmegyünk a maradékon.
+
+        for (int i = 2; i <= limit; i++)
+        {
+            T aktualisJelolt = generatorFunc(i);
+            float aktualisErtek = fitnessFunc(aktualisJelolt);
+
+            LepesSzam++; // Lépésszám növelése
+
+            // Ha jobbat találtunk, csere
+            if (aktualisErtek > maxErtek)
+            {
+                maxErtek = aktualisErtek;
+                legjobbMegoldas = aktualisJelolt;
+            }
+        }
+
+        return legjobbMegoldas;
+    }
+}
+
+public class NyersEroHatizsakPakolas
+{
+    public int LepesSzam { get; private set; }
+
+    private readonly HatizsakProblema context; // 'problema' átnevezve
+    private bool[] cachedResult; // 'legjobbPakolas' átnevezve
+
+    public NyersEroHatizsakPakolas(HatizsakProblema problema)
+    {
+        this.LepesSzam = 0;
+        this.context = problema;
     }
 
-    public class NyersEroHatizsakPakolas
+    public bool[] Generator(int index)
     {
-        private HatizsakProblema problema;
-        public int LepesSzam { get; private set; }
-        private bool[] _cachedResult;
+        int elementCount = context.n;
+        if (elementCount == 0) return new bool[0];
 
-        public NyersEroHatizsakPakolas(HatizsakProblema problema)
+        bool[] selection = new bool[elementCount];
+
+        for (int bit = 0; bit < elementCount; bit++)
         {
-            this.problema = problema;
-            this.LepesSzam = 0;
+            // MÁSIK LOGIKA:
+            // Az eredeti: (i & (1 << j)) != 0
+            // Ez: jobbra toljuk a biteket és megnézzük az utolsót. Matematikailag ugyanaz, de a kód más.
+            selection[bit] = ((index >> bit) & 1) == 1;
+        }
+        return selection;
+    }
+
+    public float Josag(bool[] pakolas)
+    {
+        // Tömörített logika: ternáris operátor használata if-else helyett
+        return context.Ervenyes(pakolas) ? context.OsszErtek(pakolas) : -1f;
+    }
+
+    public bool[] OptimalisMegoldas()
+    {
+        if (context.n == 0)
+        {
+            cachedResult = new bool[0];
+            LepesSzam = 0;
+            return cachedResult;
         }
 
-        public bool[] Generator(int i)
+        // Bitshift használata a hatványozás helyett (1 << n ugyanaz mint 2^n)
+        int range = 1 << context.n;
+
+        // Generic példányosítása
+        var solver = new NyersEro<bool[]>(range, Generator, Josag);
+
+        cachedResult = solver.OptimalisMegoldas();
+
+        // Frissítjük a lépésszámot
+        LepesSzam = solver.LepesSzam;
+
+        return cachedResult;
+    }
+
+    public float OptimalisErtek()
+    {
+        // Null coalescing ellenőrzés
+        if (cachedResult == null)
         {
-            bool[] pakolas = new bool[problema.n];
-            for (int bitIndex = 0; bitIndex < problema.n; bitIndex++)
-            {
-                pakolas[bitIndex] = ((i >> bitIndex) & 1) == 1;
-            }
-            return pakolas;
+            OptimalisMegoldas();
         }
-
-        public float Josag(bool[] pakolas)
-        {
-            if (problema.Ervenyes(pakolas))
-            {
-                return problema.OsszErtek(pakolas);
-            }
-            return -1f;
-        }
-
-        public bool[] OptimalisMegoldas()
-        {
-            // 2 a hatodikon helyett bitshift: 1 << n
-            int m = 1 << problema.n;
-
-            var nyersEroAlgoritmus = new NyersEro<bool[]>(m, Generator, Josag);
-
-            _cachedResult = nyersEroAlgoritmus.OptimalisMegoldas();
-            LepesSzam = nyersEroAlgoritmus.LepesSzam;
-
-            return _cachedResult;
-        }
-
-        public float OptimalisErtek()
-        {
-            if (_cachedResult == null)
-            {
-                OptimalisMegoldas();
-            }
-            return problema.OsszErtek(_cachedResult);
-        }
+        return context.OsszErtek(cachedResult);
     }
 }
